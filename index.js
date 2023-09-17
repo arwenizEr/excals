4d96c4c4e662fa44f5555161b96e0825d8241579
arwenizEr <kanbermert@gmail.com>
2023-09-17 14:24:45 +0300

    update

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const INDEX = '/index.html';
 const server = app()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://excals-c58d60f38aa8.herokuapp.com"],
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
