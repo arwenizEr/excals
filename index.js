5a1cf6201f24178ae579b4fc1c3276257f6a4d49
arwenizEr <kanbermert@gmail.com>
2023-09-17 14:35:45 +0300

    update

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const io = socketIO(server, {
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 
